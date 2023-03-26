7e6b41ca7f9d192bf774d9ea2592d084f85a493c
JHL <jhlopen@outlook.com>
2023-03-26 13:51:35 +0200

    backend: Import GraphQL packages

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RandomNumbersModule } from './random-numbers/random-numbers.module';
 
 @Module({
-  imports: [RandomNumbersModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/schema.gql',
+    }),
+    RandomNumbersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
