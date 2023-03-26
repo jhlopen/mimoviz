1f9ab674a28b66f9f2bbdb8ebb07311bb0661558
JHL <jhlopen@outlook.com>
2023-03-27 00:01:09 +0200

    frontend: Add stream chart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import DataTabs from "./components/DataTabs";
@@ -5,7 +6,9 @@ import DataTabs from "./components/DataTabs";
 function App() {
   return (
     <div className="App">
-      <DataTabs />
+      <Container fluid>
+        <DataTabs />
+      </Container>
     </div>
   );
 }
--- frontend/src/components/VisualizationTabs.tsx
+++ frontend/src/components/VisualizationTabs.tsx
@@ -7,13 +7,25 @@ import {
   NavItem,
   NavLink,
 } from "react-bootstrap";
+import Stream from "./Stream";
 
 export default function VisualizationTabs() {
   return (
     <TabContainer id="visualization-tabs" defaultActiveKey="stream">
       <Row>
         <TabContent>
-          <TabPane eventKey="stream"></TabPane>
+          <TabPane eventKey="stream" style={{ height: "500px" }}>
+            <Stream
+              data={[
+                { a: 1, b: 1, c: 1 },
+                { a: 1, b: 2, c: 3 },
+                { a: 3, b: 2, c: 1 },
+              ]}
+              keys={["a", "b", "c"]}
+              bottomAxisLegend="X"
+              leftAxisLegend="Y"
+            />
+          </TabPane>
           <TabPane eventKey="line"></TabPane>
           <TabPane eventKey="bar"></TabPane>
         </TabContent>
