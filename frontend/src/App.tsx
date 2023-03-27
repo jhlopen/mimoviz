a329a86e15c25ccb8377a83c0318ef7e5fa166ba
JHL <jhlopen@outlook.com>
2023-03-27 02:08:30 +0200

    frontend: Integrate Apollo Client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import { Container } from "react-bootstrap";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo-client";
+import DataTabs from "./components/DataTabs";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import DataTabs from "./components/DataTabs";
 
 function App() {
   return (
-    <div className="App">
+    <ApolloProvider client={client}>
       <Container fluid>
         <DataTabs />
       </Container>
-    </div>
+    </ApolloProvider>
   );
 }
 
